c20c51dcc2e158d8231892b7ed1dc294e13fd36b
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-07-14 12:54:29 +1000

    linked s3 to backend

---
+++ backend/routes/image-upload.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../services/file-upload');
+
+const singleImageUpload = upload.single('image');
+
+router.post('/image-upload', function (req, res) {
+  singleImageUpload(req, res, (error) => {
+    if (error) {
+      return res
+        .status(422)
+        .send({
+          errors: [{ title: 'Invalid File Type', detail: error.message }]
+        });
+    }
+    return res.json({ imageUrl: req.file.location });
+  });
+});
+
+module.exports = router;
