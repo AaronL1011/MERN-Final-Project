4303a8e78cbce40befbd18736ef11a9a9d8b9ba0
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-07-15 14:30:25 +1000

    started post routes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const verify = require('../utils/verify-token');
+const Post = require('../models/post');
 
-router.get('/', verify, (req, res) => {
-  res.send(req.user);
+// Get all posts - PUBLIC ROUTE
+router.get('/', async (req, res) => {
+  const posts = await Post.find();
+  res.status(200).send(posts);
 });
 
 module.exports = router;
