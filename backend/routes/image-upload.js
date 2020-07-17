3eae0fafafece9c66e79ff5b538ce0ac97048af7
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-07-17 15:42:16 +1000

    implimented multiple file upload

--- backend/routes/image-upload.js
+++ backend/routes/image-upload.js
@@ -2,16 +2,19 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../utils/file-upload');
 
-const singleImageUpload = upload.single('image');
+const imageUpload = upload.array('images', 4);
 
 router.post('/image-upload', function (req, res) {
-  singleImageUpload(req, res, (error) => {
+  console.log(req.files);
+  imageUpload(req, res, (error) => {
     if (error) {
       return res.status(422).send({
         errors: [{ title: 'Invalid File Type', detail: error.message }]
       });
     }
-    return res.json({ imageUrl: req.file.location });
+    const urls = req.files.map((file) => file.location);
+    console.log(urls);
+    return res.json({ image: req.files });
   });
 });
 
