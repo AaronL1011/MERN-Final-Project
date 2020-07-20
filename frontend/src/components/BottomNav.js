2b668d00ec19f96520d2aba363fa9f0667e622df
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-07-20 11:43:45 +1000

    added material ui and confirmed working

---
+++ frontend/src/components/BottomNav.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import FolderIcon from '@material-ui/icons/Folder';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+const useStyles = makeStyles({
+  root: {
+    width: 500,
+    background: '#525252'
+  },
+  iconcolor: {
+    color: '#d4d4d4'
+  }
+});
+
+export default function LabelBottomNavigation() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState('recents');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={handleChange}
+      className={classes.root}
+    >
+      <BottomNavigationAction
+        label='Recents'
+        value='recents'
+        icon={<RestoreIcon />}
+        className={classes.iconcolor}
+      />
+      <BottomNavigationAction
+        label='Favorites'
+        value='favorites'
+        icon={<FavoriteIcon />}
+        className={classes.iconcolor}
+      />
+      <BottomNavigationAction
+        label='Nearby'
+        value='nearby'
+        icon={<LocationOnIcon />}
+        className={classes.iconcolor}
+      />
+      <BottomNavigationAction
+        label='Folder'
+        value='folder'
+        icon={<FolderIcon />}
+        className={classes.iconcolor}
+      />
+    </BottomNavigation>
+  );
+}
