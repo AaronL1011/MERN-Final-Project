06e0f43d67de33ed7dfb3eff6943191abc5a01c0
Evelyn Paplauskas <evelyn.paplauskas@outlook.com>
2020-07-22 11:56:37 +1000

    Add display view toggle to searchbar component

--- frontend/src/components/Searchbar.js
+++ frontend/src/components/Searchbar.js
@@ -1,24 +1,74 @@
-import React, { useState } from 'react';
-import { Box, TextField, Button } from "@material-ui/core";
+import React, { useState } from "react";
+import { Grid, Box, TextField, Button } from "@material-ui/core";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
 import SearchIcon from "@material-ui/icons/Search";
+import ViewDayIcon from "@material-ui/icons/ViewDay";
+import ViewModuleIcon from "@material-ui/icons/ViewModule";
 
 const Searchbar = () => {
-    const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
+  const [displayView, setDisplayView] = useState("single");
 
-    return (
-      <Box component="span">
-        <TextField 
-            variant="outlined"
-            id="searchbartext" 
-            label="Search"
-            autoComplete="on"
-            onChange={e => {setSearchText(e.target.value)}}
+  const runSearch = (text) => {
+    console.log(`${text}`);
+  };
+
+  const handleDisplayView = (event, newDisplayView) => {
+    if (newDisplayView !== null) {
+      setDisplayView(newDisplayView);
+    }
+  };
+
+  return (
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justify="space-between"
+    >
+      <Grid item>
+        <TextField
+          variant="outlined"
+          id="searchbartext"
+          label="Search"
+          autoComplete="on"
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
         />
-        <Button>
-            <SearchIcon />
+        <Button
+          onClick={(event) => {
+            runSearch(searchText);
+          }}
+        >
+          <SearchIcon />
         </Button>
-      </Box>
-    );
+      </Grid>
+
+      <Grid item>
+        <ToggleButtonGroup
+          value={displayView}
+          exclusive
+          onChange={handleDisplayView}
+          aria-label="display alignment"
+        >
+          <ToggleButton
+            value="single"
+            aria-label="display results in single column"
+          >
+            <ViewDayIcon />
+          </ToggleButton>
+          <ToggleButton
+            value="multiple"
+            aria-label="display results in multiple columns"
+          >
+            <ViewModuleIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Searchbar;
