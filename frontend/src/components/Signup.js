ba0af93414e038a73b5e29db2f6a2cefb94cb9be
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-07-22 11:34:08 +1000

    added edit profile component

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, CircularProgress, TextField, Button } from '@material-ui/core';
+import {
+  Box,
+  Grid,
+  CircularProgress,
+  TextField,
+  Button
+} from '@material-ui/core';
 import CameraAltIcon from '@material-ui/icons/CameraAlt';
 import Alert from '@material-ui/lab/Alert';
 import axios from 'axios';
@@ -46,139 +52,147 @@ const Signup = () => {
   return (
     <>
       {!isLoading ? (
-        <Grid
-          container
-          direction='column'
-          alignItems='center'
-          justify='center'
-          spacing={1}
-        >
-          <Grid item container alignItems='center' direction='column'>
-            <h1>
-              GrupGrup <CameraAltIcon fontSize={'large'} />
-            </h1>
-            {errorMessage && <Alert severity='error'>{errorMessage}</Alert>}
-          </Grid>
+        <Box height='100%' display='flex' alignItems='center'>
           <Grid
-            item
             container
+            direction='column'
             alignItems='center'
             justify='center'
-            xs={11}
-            sm={6}
-            lg={3}
-            xl={2}
+            spacing={1}
           >
-            <TextField
-              id='username-field'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              label='Username'
-              variant='outlined'
-              fullWidth
-            />
+            <Grid item container alignItems='center' direction='column'>
+              <h1>
+                GrupGrup <CameraAltIcon fontSize={'large'} />
+              </h1>
+              {errorMessage && <Alert severity='error'>{errorMessage}</Alert>}
+            </Grid>
+            <Grid
+              item
+              container
+              alignItems='center'
+              justify='center'
+              xs={11}
+              sm={6}
+              lg={3}
+              xl={2}
+            >
+              <TextField
+                id='username-field'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                label='Username'
+                variant='outlined'
+                fullWidth
+              />
+            </Grid>
+            <Grid
+              item
+              container
+              alignItems='center'
+              justify='center'
+              xs={11}
+              sm={6}
+              lg={3}
+              xl={2}
+            >
+              <TextField
+                id='email-field'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                label='Email'
+                type='email'
+                variant='outlined'
+                fullWidth
+              />
+            </Grid>
+            <Grid
+              item
+              container
+              alignItems='center'
+              justify='center'
+              xs={11}
+              sm={6}
+              lg={3}
+              xl={2}
+            >
+              <TextField
+                id='password-field'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                label='Password'
+                type='password'
+                variant='outlined'
+                fullWidth
+              />
+            </Grid>
+            <Grid
+              item
+              container
+              alignItems='center'
+              justify='center'
+              xs={11}
+              sm={6}
+              lg={3}
+              xl={2}
+            >
+              <TextField
+                id='confirm-password-field'
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                label='Confirm Password'
+                type='password'
+                variant='outlined'
+                fullWidth
+              />
+            </Grid>
+            <Grid
+              item
+              container
+              alignItems='center'
+              justify='center'
+              xs={11}
+              sm={6}
+              lg={3}
+              xl={2}
+            >
+              <Button
+                onClick={() => handleLogin()}
+                variant='outlined'
+                fullWidth
+              >
+                Create Account
+              </Button>
+            </Grid>
+            <Grid
+              item
+              container
+              alignItems='center'
+              justify='center'
+              xs={11}
+              sm={6}
+              lg={3}
+              xl={2}
+            >
+              <Link to='/login' style={styles.link}>
+                Have an account? Log In
+              </Link>
+            </Grid>
           </Grid>
-          <Grid
-            item
-            container
-            alignItems='center'
-            justify='center'
-            xs={11}
-            sm={6}
-            lg={3}
-            xl={2}
-          >
-            <TextField
-              id='email-field'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              label='Email'
-              type='email'
-              variant='outlined'
-              fullWidth
-            />
-          </Grid>
-          <Grid
-            item
-            container
-            alignItems='center'
-            justify='center'
-            xs={11}
-            sm={6}
-            lg={3}
-            xl={2}
-          >
-            <TextField
-              id='password-field'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              label='Password'
-              type='password'
-              variant='outlined'
-              fullWidth
-            />
-          </Grid>
-          <Grid
-            item
-            container
-            alignItems='center'
-            justify='center'
-            xs={11}
-            sm={6}
-            lg={3}
-            xl={2}
-          >
-            <TextField
-              id='confirm-password-field'
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              label='Confirm Password'
-              type='password'
-              variant='outlined'
-              fullWidth
-            />
-          </Grid>
-          <Grid
-            item
-            container
-            alignItems='center'
-            justify='center'
-            xs={11}
-            sm={6}
-            lg={3}
-            xl={2}
-          >
-            <Button onClick={() => handleLogin()} variant='outlined' fullWidth>
-              Create Account
-            </Button>
-          </Grid>
-          <Grid
-            item
-            container
-            alignItems='center'
-            justify='center'
-            xs={11}
-            sm={6}
-            lg={3}
-            xl={2}
-          >
-            <Link to='/login' style={styles.link}>
-              Have an account? Log In
-            </Link>
-          </Grid>
-        </Grid>
+        </Box>
       ) : (
-        <Grid
-          container
-          direction='column'
-          alignItems='center'
-          justify='center'
-          spacing={1}
-        >
-          <Grid item padding={0}>
-            <CircularProgress />
+        <Box height='100%' display='flex' alignItems='center'>
+          <Grid
+            container
+            direction='column'
+            alignItems='center'
+            justify='center'
+            spacing={1}
+          >
+            <Grid item padding={0}>
+              <CircularProgress />
+            </Grid>
           </Grid>
-        </Grid>
+        </Box>
       )}
     </>
   );
